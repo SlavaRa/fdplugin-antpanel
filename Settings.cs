82302190afd8ca37b11544aba2f7641ef0218d99
slavara <ratdg@ya.ru>
2014-05-21 17:13:09 +0400

    Cleanup...

--- Settings.cs
+++ Settings.cs
@@ -8,15 +8,14 @@ namespace AntPlugin
     [Serializable]
     public class Settings
     {
-        private const String DEFAULT_ANT_PATH = "";
-        
-        private String antPath = DEFAULT_ANT_PATH;
+        private const string DEFAULT_ANT_PATH = "";
+        private string antPath = DEFAULT_ANT_PATH;
 
         [DisplayName("Path to Ant")]
         [Description("Path to Ant installation dir")]
         [DefaultValue(DEFAULT_ANT_PATH)]
         [Editor(typeof(FolderNameEditor), typeof(UITypeEditor))]
-        public String AntPath
+        public string AntPath
         {
             get { return antPath; }
             set { antPath = value; }
