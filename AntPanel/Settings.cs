651262b8e5451853a87daff8b22ec0656afd6066
slavara <ratdg@ya.ru>
2014-09-28 17:40:10 +0400

    Removing extra variables...

--- AntPanel/Settings.cs
+++ AntPanel/Settings.cs
@@ -8,18 +8,10 @@ namespace AntPanel
     [Serializable]
     public class Settings
     {
-        private const string DEFAULT_ANT_PATH = "";
-        private string antPath = DEFAULT_ANT_PATH;
-
         [DisplayName("Path to Ant")]
         [Description("Path to Ant installation dir")]
-        [DefaultValue(DEFAULT_ANT_PATH)]
+        [DefaultValue("")]
         [Editor(typeof(FolderNameEditor), typeof(UITypeEditor))]
-        public string AntPath
-        {
-            get { return antPath; }
-            set { antPath = value; }
-        }
-
+        public string AntPath { get; set; }
     }
 }
