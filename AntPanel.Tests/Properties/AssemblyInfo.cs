68fffd062c3b892558638985d5f7f0b17c3fd3a5
slavara <ratdg@ya.ru>
2015-01-12 13:54:24 +0300

    Minor...

--- AntPanel.Tests/Properties/AssemblyInfo.cs
+++ AntPanel.Tests/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tests of AntPanel Plugin For FlashDevelop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AntPanel.Tests")]
+[assembly: AssemblyDescription("Tests of AntPanel Plugin For FlashDevelop")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SlavaRa")]
 [assembly: AssemblyProduct("AntPanel.Tests")]
